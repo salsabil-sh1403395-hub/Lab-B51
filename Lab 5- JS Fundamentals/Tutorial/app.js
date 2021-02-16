e33a7fc744083f68c83fd733a398cb3ef6dc3e5f
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-16 18:10:46 +0300

    Update app.js

--- Lab 5- JS Fundamentals/Tutorial/app.js
+++ Lab 5- JS Fundamentals/Tutorial/app.js
@@ -138,7 +138,7 @@ display(44, 55, (x, y) => x - y)
 */
 
 //Array
-let names = ["Muram", "Asma" , "Amna", "Alnoud" , "Joud"]
+let names = ["Muram First", "Asma Second", "Amna Third", "Alnoud Fourth", "Joud Firth"]
 let result = names.join(" - ")
 console.log(result)
 
@@ -146,41 +146,73 @@ console.log(result)
 
 //Add element to the end
 names.push("Sharefa")
-console.log('After push' , names)
+console.log('After push', names)
 
 //Add element to the beginning
 names.unshift("Sumayya")
-console.log('After unshift' , names)
+console.log('After unshift', names)
 
 //Remove element from the end
 
 names.pop()
-console.log('After popping' , names)
+console.log('After popping', names)
 
 //Remove element to the beginning
 names.shift()
-console.log('After Shifting' , names)
+console.log('After Shifting', names)
+
 //remove element from specific location
 names.splice(3, 1)
-console.log('After Shifting' , names)
+console.log('After Shifting', names)
 
 //sort elements
-
 names.sort()
-console.log('After Sorting' , names)
+console.log('After Sorting', names)
 
 
 names.sort().reverse()
-console.log('After Sorting' , names)
+console.log('After Sorting', names)
 
-//filter elements
-let x = names.filter(x => x.length < 5)
-console.log('After Filtering' , x)
+
+//multiple elementes
+let namesStartWithA = names.filter(x => x.length < 10)
+console.log('After Filtering', namesStartWithA)
+
+//find one element that meets
+let oneElement = names.find(x => x.length < 10)
+console.log('After finding', oneElement)
 //map
 
-let namesStartWithA = names.filter(x => x.length < 5)
-console.log('After Filtering' , namesStartWithA)
-
-//find
+let mappedArray = names.map(x => `${x} : ${x.length}`)
+console.log('After mapping', mappedArray)
 
 // reduce
+
+function reduce(prev, current) {
+    if (prev.length > current.length)
+        return prev
+    else
+        return current
+
+
+}
+
+let longestName = names.reduce((prev, current) => prev.length >= current.length ? prev : current)
+console.log(longestName)
+//flatten
+
+let countries = [['Qatar', ['France', 'Sweden'], 'Kuwait'], 'Germany', 'Italy']
+console.log(countries)
+console.log(countries.flat(2))
+
+
+let numbers = [11, 21, 13, 4 ,111 ,1134]
+
+
+
+// (x, y) => x - y)
+
+numbers.splice(1, 0, 89)
+
+console.log('wrong way to sort number ' , numbers.sort())
+console.log('proper way' , numbers.sort((x, y) => y - x))
