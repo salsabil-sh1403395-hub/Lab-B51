fccfee3cf1a7e580d2e753dcc9cac5925c93e191
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-16 17:37:07 +0300

    Update app.js

--- Lab 5- JS Fundamentals/Tutorial/app.js
+++ Lab 5- JS Fundamentals/Tutorial/app.js
@@ -82,7 +82,7 @@ let result = add(45,66)
 console.log(`Result = ${result}`)
 */
 //Function as first class citizens
-
+/*
 function multiply(x, y) {return x * y}
 function sub(x, y) {return x - y}
 
@@ -95,8 +95,92 @@ display(20, 55, function(x, y) {return x + y})
 display(44, 55, function(x, y) {return x / y})
 display(44, 55, multiply)
 display(44, 55, sub)
-
+*/
 //Arrow Function
 
+// function type1(){console.log('One single like')}
+// () => console.log('One single like')
+//
+// function type11(){return 20*40}
+// () => 20*40
+//
+// function type2(x){console.log(x)}
+// x => console.log(x)
+//
+// function type22(x){return x*99}
+// x => x * 99
+//
+// function type2(x,y){ return x+y}
+// (x,y) =>  x+y
+//
+//
+// (x , y)=>{
+//     let result = x+y
+//     return result
+// }
+/*
+function multiply(x, y) {
+    return x * y
+}
+
+function sub(x, y) {
+    return x - y
+}
+
+let display = function (x, y, anotherFunction) {
+    let result = anotherFunction(x, y)
+    console.log(`Result = ${result}`)
+}
+display(20, 55, (x, y) => x + y)
+display(44, 55, (x, y) => x / y)
+display(44, 55, (x, y) => x * y)
+display(44, 55, (x, y) => x - y)
+*/
+
+//Array
+let names = ["Muram", "Asma" , "Amna", "Alnoud" , "Joud"]
+let result = names.join(" - ")
+console.log(result)
 
 //Arrays and Methods
+
+//Add element to the end
+names.push("Sharefa")
+console.log('After push' , names)
+
+//Add element to the beginning
+names.unshift("Sumayya")
+console.log('After unshift' , names)
+
+//Remove element from the end
+
+names.pop()
+console.log('After popping' , names)
+
+//Remove element to the beginning
+names.shift()
+console.log('After Shifting' , names)
+//remove element from specific location
+names.splice(3, 1)
+console.log('After Shifting' , names)
+
+//sort elements
+
+names.sort()
+console.log('After Sorting' , names)
+
+
+names.sort().reverse()
+console.log('After Sorting' , names)
+
+//filter elements
+let x = names.filter(x => x.length < 5)
+console.log('After Filtering' , x)
+//map
+
+let namesStartWithA = names.filter(x => x.length < 5)
+console.log('After Filtering' , namesStartWithA)
+
+//find
+
+// reduce
