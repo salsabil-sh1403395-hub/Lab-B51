1fcc98f43a2d585c5be854bce67b585173db44a6
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-02 17:08:05 +0300

    update 5:08

--- Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
@@ -5,12 +5,41 @@ const countryInputBox = document.querySelector('#country')
 const populationInputBox = document.querySelector('#population')
 
 const formElement = document.querySelector('#form')
+const countriesTable = document.querySelector('#countries')
+
+const db = new Localbase('b52.census.db')
+showCensusData()
+
 
 //Step 2 - Add events
 formElement.addEventListener('submit', addCensus)
-//database
 
-const db = new Localbase('b52.census.db')
+async function showCensusData(){
+    //get all the collection from the databas
+    const censusData= await db.collection('census').get()
+    const censusRows = censusData.map(c => censusToHTMLRow(c))
+    countriesTable.innerHTML = censusRows.join('')
+}
+
+function censusToHTMLRow(c) {
+    return `
+        <tr>
+            <td>${c.country}</td>
+            <td>${c.population}</td> 
+            <td></td>
+        </tr>
+    `
+}
+
+async function addCensus(event) {
+    event.preventDefault()
+    const newCensus = form2Object(formElement)
+    newCensus.id = Date.now().toString()
+
+    const message = await db.collection('census').add(newCensus)
+    formElement.reset()
+    await showCensusData()
+}
 
 function form2Object(formElement) {
     const formData = new FormData(formElement)
@@ -20,15 +49,4 @@ function form2Object(formElement) {
     }
     return data
 }
-
-async function addCensus(event) {
-    event.preventDefault()
-    const newCensus = form2Object(formElement)
-    newCensus.id = Date.now().toString()
-
-    //asynchronous
-    const message = await db.collection('census').add(newCensus)
-    console.log(message)
-}
-
 
