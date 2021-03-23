55dae5e671d45de7b5b208c9917f0984c3415b4b
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-23 16:14:45 +0300

    host static data on the server
    Implemented the hosting of static content on the server

--- Lab 9 Web API/BankingApp/app.js
+++ Lab 9 Web API/BankingApp/app.js
@@ -3,6 +3,12 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+
+//return static and dynamic
+//add this middleware that tells the server where the static data is located
+
+app.use(express.static('public'))
+
 app.listen(port, () => {
     console.log(`Server started @http://localhost:${port} successfully `)
 })
