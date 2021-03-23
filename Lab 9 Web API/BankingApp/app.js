c500a45a88fc420e6f70f8c59d8ae644e3ed2133
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-23 16:56:15 +0300

    Created the crud operations

--- Lab 9 Web API/BankingApp/app.js
+++ Lab 9 Web API/BankingApp/app.js
@@ -1,14 +1,38 @@
 import express from 'express'
+import morgan from 'morgan'
 //require
 const app = express()
 const port = 3000
 
+//Buying car [base model]
 
 //return static and dynamic
 //add this middleware that tells the server where the static data is located
 
+
 app.use(express.static('public'))
 
-app.listen(port, () => {
-    console.log(`Server started @http://localhost:${port} successfully `)
+app.use(morgan('dev'))
+//dynamic [web APIs]
+
+// CRUD
+//Reading
+app.get('/api/accounts' , (req, res)=>{
+    res.send('This is the get method for /api/accounts')
+})
+//adding , creating
+app.post('/api/accounts' , (req, res)=>{
+    res.send('This is the post method for /api/accounts')
+})
+//updating
+app.put('/api/accounts' , (req, res)=>{
+    res.send('This is the put method for /api/accounts')
+})
+//deleting or removing
+app.delete('/api/accounts' , (req, res)=>{
+    res.send('This is the delete method for /api/accounts')
+})
+
+app.listen(port, (req,res) => {
+    console.log(`Server started @http://localhost:${port} successfully`)
 })
